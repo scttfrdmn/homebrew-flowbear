c2a2b1baa45c1ff4fd761f67a57ab256a129c62e
Scott Friedman <3011922+scttfrdmn@users.noreply.github.com>
2025-11-08 14:25:08 -0800

    chore: Update formula for v0.2.0 release
    Updated:
    - Version to 0.2.0
    - SHA256 checksums for darwin-amd64 and darwin-arm64 binaries
    
    Checksums verified from GitHub release v0.2.0.

--- Formula/flowbear.rb
+++ Formula/flowbear.rb
@@ -1,15 +1,15 @@
 class Flowbear < Formula
   desc "The Anti-Ghostwriter - Privacy-first writing assistant"
   homepage "https://github.com/scttfrdmn/flowbear"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/scttfrdmn/flowbear/releases/download/v#{version}/flowbear-#{version}-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "decdf473373d88cb13da0de26ab132d087ae9b140e8f271c1dfa3c14ad7afc21"
     else
       url "https://github.com/scttfrdmn/flowbear/releases/download/v#{version}/flowbear-#{version}-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_AMD64_SHA256"
+      sha256 "60567898dd69dd04f613f7988594e92ec038b27be3cd45ba1eb734e0e9ddf522"
     end
   end
 
